cc33d1296d625c64355f17f41fb515f634cd34ab
TanmayGadge <tanmaygadge9273@gmail.com>
2021-04-04 19:27:34 +0530

    AirBalloonRide

---
+++ functions.js
@@ -0,0 +1,16 @@
+function updateHeight(x, y){
+    database.ref('balloon/height').set({
+        'x': height.x + x,
+        'y': height.y + y
+    })
+}
+
+function readHeight(data){
+    height = data.val()
+    balloon.x = height.x;
+    balloon.y = height.y;
+}
+
+function showError(){
+    console.log("Error in writting in the database");
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,37 @@
+var database, position;
+var balloon;
+var bgImg, balloonImg1 ,balloonImg2, balloonImg3;
+
+function preload(){
+  bgImg = loadImage('pro-C35 images/Hot Air Ballon-01.png');
+
+  balloonImg1 = loadImage('pro-C35 images/Hot Air Ballon-02.png');
+  ballooonImg2 = loadImage('pro-C35 images/Hot Air Ballon-03.png');
+  balloonImg3 = loadImage('pro-C35 images/Hot Air Ballon-04.png');
+  
+  balloonAnimation = loadAnimation('pro-C35 images/Hot Air Ballon-02.png',
+  'pro-C35 images/Hot Air Ballon-03.png',
+  'pro-C35 images/Hot Air Ballon-04.png');
+}
+
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  var balloonPosition = database.ref('balloon/height');
+  balloonPosition.on("value", readPosition, showError);
+
+  balloon = createSprite();
+
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(bgImg);
+
+  if(keyDown(UP_ARROW)){
+    updateHeight(0, -10);
+    balloon.addAnimation("HotAirBalloon",balloonAnimation);
+    balloon.scale -= 0.01;
+  }
+
 }
