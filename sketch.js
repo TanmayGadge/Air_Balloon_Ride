3aff3e5ca8d53e647e1c54b9b3f3d4435453fca1
TanmayGadge <tanmaygadge9273@gmail.com>
2021-04-08 19:42:07 +0530

    AirBalloon

--- sketch.js
+++ sketch.js
@@ -1,37 +1,64 @@
 var database, position;
 var balloon;
-var bgImg, balloonImg1 ,balloonImg2, balloonImg3;
+var bgImg, balloonImg1, balloonImg2, balloonImg3;
 
-function preload(){
+function preload() {
   bgImg = loadImage('pro-C35 images/Hot Air Ballon-01.png');
 
   balloonImg1 = loadImage('pro-C35 images/Hot Air Ballon-02.png');
   ballooonImg2 = loadImage('pro-C35 images/Hot Air Ballon-03.png');
   balloonImg3 = loadImage('pro-C35 images/Hot Air Ballon-04.png');
-  
-  balloonAnimation = loadAnimation('pro-C35 images/Hot Air Ballon-02.png',
-  'pro-C35 images/Hot Air Ballon-03.png',
-  'pro-C35 images/Hot Air Ballon-04.png');
+
+  balloonAnimation = loadAnimation(
+    'pro-C35 images/Hot Air Ballon-02.png',
+    'pro-C35 images/Hot Air Ballon-02.png',
+    'pro-C35 images/Hot Air Ballon-02.png',
+    'pro-C35 images/Hot Air Ballon-03.png',
+    'pro-C35 images/Hot Air Ballon-03.png',
+    'pro-C35 images/Hot Air Ballon-03.png',
+    'pro-C35 images/Hot Air Ballon-04.png',
+    'pro-C35 images/Hot Air Ballon-04.png',
+    'pro-C35 images/Hot Air Ballon-04.png'
+  );
 }
 
 
 function setup() {
-  createCanvas(800,400);
+  database = firebase.database();
+  createCanvas(800, 400);
+  balloon = createSprite(400, 200);
+  balloon.addAnimation("HotAirBalloon", balloonImg1);
+  // balloon.addAnimation("HotAirBalloon", balloonAnimation);
+  balloon.scale = 0.5;
 
   var balloonPosition = database.ref('balloon/height');
-  balloonPosition.on("value", readPosition, showError);
-
-  balloon = createSprite();
+  balloonPosition.on("value", readHeight, showError);
 
 }
 
 function draw() {
   background(bgImg);
-
-  if(keyDown(UP_ARROW)){
-    updateHeight(0, -10);
-    balloon.addAnimation("HotAirBalloon",balloonAnimation);
-    balloon.scale -= 0.01;
+  if (height !== undefined) {
+    if (keyDown(UP_ARROW)) {
+      updateHeight(0, -10);
+      balloon.addAnimation("HotAirBalloon", balloonAnimation);
+      balloon.scale -= 0.005;
+    }
+    if (keyDown(DOWN_ARROW)) {
+      updateHeight(0, 10);
+      balloon.addAnimation("HotAirBalloon", balloonAnimation);
+      balloon.scale += 0.005;
+    }
+    if (keyDown(LEFT_ARROW)) {
+      updateHeight(-10, 0);
+      balloon.addAnimation("HotAirBalloon", balloonAnimation);
+      
+    }
+    if (keyDown(RIGHT_ARROW)) {
+      updateHeight(10, 0);
+      balloon.addAnimation("HotAirBalloon", balloonAnimation);
+      
+    }
   }
-
+  drawSprites();
 }
